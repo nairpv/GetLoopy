02cd058579d1461299b2d82a0cb11a0eb17ebcc4
nairpv <nairpv@mail.uc.edu>
2024-10-14 00:43:06 -0400

    Initial commit

---
+++ src/Task1-4.java
@@ -0,0 +1,28 @@
+class Task1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+class Task2 {
+    public static void main(String[] args) {
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
+class Task3 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+    }
+}
+class Task4 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
